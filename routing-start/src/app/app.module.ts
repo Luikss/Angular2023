24c3b795e6cd7bddbaf7a13fa76e639d65301e8c
Andre Luige <Andre.Luige@nortal.com>
2023-05-24 17:20:06 +0300

    Section 11: Changing Pages with Routing - Episode 141-154

--- routing-start/src/app/app.module.ts
+++ routing-start/src/app/app.module.ts
@@ -1,27 +1,22 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
-import {RouterModule, Routes, RouterLinkActive, RouterLink} from "@angular/router";
+import {AppRoutingModule} from "./app-routing.module";
 
 import { AppComponent } from './app.component';
+import { AuthGuardService } from "./auth-guard.service";
+import { AuthService } from "./auth.service";
 import { HomeComponent } from './home/home.component';
+import { CanDeactivateGuard } from "./servers/edit-server/can-deactivate-guard.service";
+import { ServerResolverService } from "./servers/server-resolver.service";
 import { UsersComponent } from './users/users.component';
 import { ServersComponent } from './servers/servers.component';
 import { UserComponent } from './users/user/user.component';
 import { EditServerComponent } from './servers/edit-server/edit-server.component';
 import { ServerComponent } from './servers/server/server.component';
 import { ServersService } from './servers/servers.service';
-
-const appRoutes: Routes = [
-  { path: '', component: HomeComponent },
-  { path: 'users', component: UsersComponent, children: [
-      { path: ':id/:name', component: UserComponent }
-    ] },
-  { path: 'servers', component: ServersComponent, children: [
-      { path: ':id', component: ServerComponent },
-      { path: ':id/edit', component: EditServerComponent }
-    ] }
-]
+import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
+import { ErrorPageComponent } from './error-page/error-page.component';
 
 @NgModule({
   declarations: [
@@ -31,17 +26,22 @@ const appRoutes: Routes = [
     ServersComponent,
     UserComponent,
     EditServerComponent,
-    ServerComponent
+    ServerComponent,
+    PageNotFoundComponent,
+    ErrorPageComponent
   ],
   imports: [
     BrowserModule,
     FormsModule,
-    RouterModule.forRoot(appRoutes),
-    RouterLinkActive,
-    RouterLink,
-    RouterLink
+    AppRoutingModule
   ],
-  providers: [ServersService],
+  providers: [
+    ServersService,
+    AuthService,
+    AuthGuardService,
+    CanDeactivateGuard,
+    ServerResolverService
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
