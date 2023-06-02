2bef684346c8db6b3d8129ad95849ab71e467ea4
Andre Luige <Andre.Luige@nortal.com>
2023-06-02 14:12:40 +0300

    Section 12: Course Project - Routing

--- angular-app/src/app/recipes/recipe-detail/recipe-detail.component.ts
+++ angular-app/src/app/recipes/recipe-detail/recipe-detail.component.ts
@@ -1,18 +1,36 @@
-import { Component, Input } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import { ActivatedRoute, Params, Router } from "@angular/router";
 import { ShoppingListService } from "../../shopping-list/shopping-list.service";
-import {Recipe} from "../recipe.model";
+import { Recipe } from "../recipe.model";
+import {RecipeService} from "../recipe.service";
 
 @Component({
   selector: 'app-recipe-detail',
   templateUrl: './recipe-detail.component.html',
   styleUrls: ['./recipe-detail.component.css']
 })
-export class RecipeDetailComponent {
-  @Input() recipe: Recipe;
+export class RecipeDetailComponent implements OnInit {
+  recipe: Recipe;
 
-  constructor(private shoppingListService: ShoppingListService) {}
+  constructor(private shoppingListService: ShoppingListService,
+              private recipeService: RecipeService,
+              private route: ActivatedRoute,
+              private router: Router) {}
+
+  ngOnInit() {
+    this.route.params
+      .subscribe(
+        (params: Params) => {
+          this.recipe = this.recipeService.getRecipe(+params['id']);
+        }
+      );
+  }
 
   onToShoppingList() {
     this.shoppingListService.addIngredients(this.recipe.ingredients);
+  }
+
+  onEditRecipe() {
+    this.router.navigate(['edit'], {relativeTo: this.route});
   }
 }
