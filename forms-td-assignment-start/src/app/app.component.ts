5aa5d6d9afec855697dbfe90e7517a3c233d9653
Andre Luige <Andre.Luige@nortal.com>
2023-06-07 15:59:55 +0300

    Section 15: Handling Forms in Angular Apps - Assignment 6

--- forms-td-assignment-start/src/app/app.component.ts
+++ forms-td-assignment-start/src/app/app.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild } from '@angular/core';
+import { NgForm } from "@angular/forms";
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,23 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
+  @ViewChild('f') form: NgForm;
+  defaultSubscription = 'advanced';
+  submitted = false;
+  submission = {
+    email: '',
+    subscription: '',
+    password: ''
+  }
 
+  onSubmit() {
+    this.submission.email = this.form.value.email;
+    this.submission.subscription = this.form.value.subscription;
+    this.submission.password = this.form.value.password;
+    this.form.reset();
+    this.form.form.patchValue({
+      subscription: this.defaultSubscription
+    });
+    this.submitted = true;
+  }
 }
