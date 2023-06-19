32074fdeb5ba7f8e08eb7bec61f2f3ed95eee29b
Andre Luige <Andre.Luige@nortal.com>
2023-06-19 17:23:12 +0300

    Section 15: Handling Forms in Angular Apps - Assignment 7

--- forms-reactive-assignment-start/src/app/app.component.ts
+++ forms-reactive-assignment-start/src/app/app.component.ts
@@ -1,9 +1,38 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup, Validators } from "@angular/forms";
+import { Observable } from "rxjs";
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
+  projectForm: FormGroup;
+
+  ngOnInit() {
+    this.projectForm = new FormGroup({
+      'name': new FormControl(null, Validators.required, this.forbiddenName),
+      'email': new FormControl(null, [Validators.required, Validators.email]),
+      'status': new FormControl('stable')
+    });
+  }
+
+  forbiddenName(control: FormControl): Promise<any> | Observable<any> {
+    const promise = new Promise<any>((resolve, reject) => {
+      setTimeout(() => {
+        if (control.value === 'Test') {
+          resolve({'nameIsForbidden': true});
+        } else {
+          resolve(null);
+        }
+      }, 500);
+    });
+    return promise;
+  }
+
+  onSubmit() {
+    console.log(this.projectForm);
+    this.projectForm.reset();
+  }
 }
