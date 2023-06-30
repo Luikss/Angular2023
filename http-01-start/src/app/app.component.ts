5fe0daf4c98a02f83a99b0d97b2c368827432789
Andre Luige <Andre.Luige@nortal.com>
2023-06-30 10:50:03 +0300

    Section 18: Making Http Requests - Episode 251-261

--- http-01-start/src/app/app.component.ts
+++ http-01-start/src/app/app.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
+import { map } from "rxjs/operators";
+import { Post } from "./post.model";
 
 @Component({
   selector: 'app-root',
@@ -7,22 +9,46 @@ import { HttpClient } from '@angular/common/http';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent implements OnInit {
+  baseUrl = 'https://ng-complete-guide-b49a3-default-rtdb.europe-west1.firebasedatabase.app/';
+  postsUrl = this.baseUrl + 'posts.json';
   loadedPosts = [];
+  isFetching = false;
 
   constructor(private http: HttpClient) {}
 
-  ngOnInit() {}
+  ngOnInit() {
+    this.fetchPosts();
+  }
 
-  onCreatePost(postData: { title: string; content: string }) {
-    // Send Http request
-    console.log(postData);
+  onCreatePost(postData: Post) {
+    this.http.post<{ name: string }>(this.postsUrl, postData).subscribe(response => {
+      console.log(response);
+    });
   }
 
   onFetchPosts() {
-    // Send Http request
+    this.fetchPosts();
   }
 
   onClearPosts() {
     // Send Http request
+  }
+
+  private fetchPosts() {
+    this.isFetching = true;
+    this.http.get<{ [key: string]: Post }>(this.postsUrl)
+      .pipe(map(response => {
+        const posts: Post[] = [];
+        for (const key in response) {
+          if (response.hasOwnProperty(key)) {
+            posts.push({ ...response[key], id: key });
+          }
+        }
+        return posts;
+      }))
+      .subscribe(posts => {
+        this.loadedPosts = posts;
+        this.isFetching = false;
+      });
   }
 }
