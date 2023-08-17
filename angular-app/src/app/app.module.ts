cf7aa0ca4a27e99050822bab2fd1e0fbdb8dedba
Andre Luige <Andre.Luige@nortal.com>
2023-08-17 12:07:25 +0300

    Section 21: Dynamic Components

--- angular-app/src/app/app.module.ts
+++ angular-app/src/app/app.module.ts
@@ -12,8 +12,10 @@ import { RecipesComponent } from './recipes/recipes.component';
 import { RecipeListComponent } from './recipes/recipe-list/recipe-list.component';
 import { RecipeDetailComponent } from './recipes/recipe-detail/recipe-detail.component';
 import { RecipeItemComponent } from './recipes/recipe-list/recipe-item/recipe-item.component';
+import { AlertComponent } from "./shared/alert/alert.component";
 import { DropdownDirective } from "./shared/dropdown.directive";
 import { LoadingSpinnerComponent } from "./shared/loading-spinner/loading-spinner.component";
+import { PlaceholderDirective } from "./shared/placeholder/placeholder.directive";
 import { ShoppingListComponent } from './shopping-list/shopping-list.component';
 import { ShoppingEditComponent } from './shopping-list/shopping-edit/shopping-edit.component';
 import { ShoppingListService } from "./shopping-list/shopping-list.service";
@@ -34,7 +36,9 @@ import { RecipeEditComponent } from './recipes/recipe-edit/recipe-edit.component
         RecipeStartComponent,
         RecipeEditComponent,
         AuthComponent,
-        LoadingSpinnerComponent
+        LoadingSpinnerComponent,
+        AlertComponent,
+        PlaceholderDirective
     ],
   imports: [
     BrowserModule,
